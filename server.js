9a6f9856f5216ac0aade5554f1cdbbd9966a2b0a
Jaume Córdoba <jaumec93@gmail.com>
2021-07-08 10:49:07 +0200

    heroku

--- server.js
+++ server.js
@@ -37,11 +37,9 @@ app.get('*', (req, res) => {
 });
 app.use(errorHandler);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+app.use(express.static(path.join(__dirname, "client", "build")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT || 5000, () => console.log(`Now listening for requests on port ${PORT}`));
